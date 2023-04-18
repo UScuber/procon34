67d79247220cef5d124118e7bd4dfa359fc2e0e1
tamagosushi <mitachi.ts75@gmail.com>
2023-04-18 16:59:45 +0900

    visualizer

---
+++ visualizer/Field.cpp
@@ -0,0 +1,18 @@
+﻿# include <Siv3D.hpp> // OpenSiv3D v0.6.9
+# include "Field.hpp"
+
+
+// フィールド情報の初期化
+Field::Field(size_t h, size_t w) :
+	height(h),
+	width(w),
+	grid(h, Array<char32>(w, '.')){}
+
+
+
+
+void Field::DisplayGrid(void) {
+	for (size_t i = 0; i < 6; i++) {
+		Rect((i % 3) * 50 + 100, (i / 3) * 50 + 100, 50, 50).draw();
+	}
+}
---
+++ visualizer/Field.hpp
@@ -0,0 +1,12 @@
+﻿# include <Siv3D.hpp> // OpenSiv3D v0.6.9
+
+class Field {
+public:
+	Field(size_t h, size_t w);
+	void DisplayGrid(void);
+private:
+	size_t height;
+	size_t width;
+	Array<Array<char32>> grid;
+};
+
