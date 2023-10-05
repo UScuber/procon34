63ff476d6d77165a107a256c98dcbfb9d5e0343b
sota <sota1210nittc@gmail.com>
2023-10-05 09:16:23 +0900

    fix timer

--- solver/timer.hpp
+++ solver/timer.hpp
@@ -6,6 +6,6 @@ struct StopWatch {
   const std::chrono::system_clock::time_point start_time;
   StopWatch() : start_time(std::chrono::system_clock::now()){}
   inline double get_ms() const noexcept{
-    return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - start_time).count() * 1e-6;
+    return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - start_time).count() * 1e-3;
   }
 };
